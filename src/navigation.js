e39b6582f015d0aa772281532742e2f0cfcef206
AVA030215 <avalee0215@gmail.com>
2025-01-03 14:21:59 +0900

    1. Finished the Welcome page 2. Able to move to Student /Teacher login page

---
+++ src/navigation.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#button-to-studentlogin").click(function () {
+        window.location.href = 'studentlogin.html';
+    });
+    $("#button-to-teacherlogin").click(function () {
+        window.location.href = 'teacherlogin.html';
+    });
+});
